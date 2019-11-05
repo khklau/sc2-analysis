b43412a4ea3722541a7b9c5eae210a4900a2f99d
Kean H Lau <khklau@users.noreply.github.com>
2019-11-06 09:09:59 +1100

    adding facts action module

---
+++ facts/action.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from pprint import PrettyPrinter
+
+
+ACTION_TYPES = frozenset({
+    'Build',
+    'Train',
+    'Upgrade',
+    'Research',
+    'Lift',
+    'Land',
+})
+
+
+def parse_action_string(input):
+    for action in ACTION_TYPES:
+        if input.find(action) == 0:
+            unit = input[len(action):]
+            return (action, unit)
+    return (None, None)
